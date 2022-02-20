371167865d8dc9eeb972bc6671f81d30cb8e9604
Reini Urban <rurban@cpan.org>
2022-02-20 21:09:50 +0100

    add MPH tests
      * tests/Makefile.in:
      * tests/test1.c: added for MPH tests. No -v, no stdin. handle %% and ,
      keyword lists.
      * tests/test.c: better arg handling, -v and/or infile.

--- tests/test.c
+++ tests/test.c
@@ -8,31 +8,54 @@
 #include <string.h>
 
 extern const char * in_word_set (const char *, size_t);
+#ifdef DEBUG
+unsigned int hash (const char *str, size_t len);
+#endif
 
 #define MAX_LEN 80
 
 int
 main (int argc, char *argv[])
 {
-  int  verbose = argc > 1 ? 1 : 0;
+  int  verbose = argc > 1 && !strcmp(argv[1], "-v") ? 1 : 0;
+  char *in = argc > 2 ? argv[2] : !verbose && argc > 1 ? argv[1] : NULL;
   char buf[MAX_LEN];
+  FILE *f = in ? fopen (in, "r") : stdin;
+  int ret = 0;
+#ifdef DEBUG
+  unsigned i = 0;
+#endif
 
-  while (fgets (buf, MAX_LEN, stdin))
+  while (fgets (buf, MAX_LEN, f))
     {
-      if (strlen (buf) > 0 && buf[strlen (buf) - 1] == '\n')
-        buf[strlen (buf) - 1] = '\0';
+      size_t len = strlen (buf);
+      if (len > 0 && buf[len - 1] == '\n')
+        buf[--len] = '\0';
 
-      if (in_word_set (buf, strlen (buf)))
+      if (in_word_set (buf, len))
         {
           if (verbose)
+#ifdef DEBUG
+            printf ("in word set %s (hash=%u, index=%u)\n", buf,
+                    hash (buf, len), i++);
+#else
             printf ("in word set %s\n", buf);
+#endif
         }
       else
         {
+          ret = 1;
           if (verbose)
+#ifdef DEBUG
+            printf ("NOT in word set %s (hash=%u, index=%u)\n",
+                    buf, hash (buf, len), i++);
+#else
             printf ("NOT in word set %s\n", buf);
+#endif
         }
     }
+  if (in)
+    fclose(f);
 
-  return 0;
+  return ret;
 }
